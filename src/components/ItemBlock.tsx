87722cdc678d1d358b4d5f828b6a339c89ee825a
noooey <20203065@kookmin.ac.kr>
2023-07-11 18:13:00 +0900

    [feat/#3] :sparkles: Add: Hover, Navigator

--- src/components/ItemBlock.tsx
+++ src/components/ItemBlock.tsx
@@ -4,11 +4,12 @@ import './ItemBlock.css';
 type MyComponentProps = {
     children?: ReactNode;
     style?: CSSProperties;
+    onClick?: React.MouseEventHandler<HTMLElement>
 }
 
-function ItemBlock({ children, style }: MyComponentProps) {
+function ItemBlock({ children, style, onClick }: MyComponentProps) {
   return (
-    <div className="item-block" style={style}>
+    <div className="item-block" style={style} onClick={onClick}>
       {children}
     </div>
   );
