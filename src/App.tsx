6cd34cd634e4fb952cc17eb521858cf0b4490799
noooey <20203065@kookmin.ac.kr>
2023-07-11 18:08:23 +0900

    [feat/#3] :sparkles: Add: Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Main from './pages/Main';
-import NotFound from './pages/NotFound';
 import NavBar from './components/NavBar';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Migration from './pages/Migration';
+import Auth from './pages/Auth';
+import Footer from './components/Footer';
+import './App.css';
 
 function App() {
   return (
-    <div className="App" style={{display: 'flex'}}>
-      <NavBar />
+    <div className="App">
       <BrowserRouter>
-				<Routes>
-					<Route path="/" element={<Main />}></Route>
-					{/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
-					<Route path="*" element={<NotFound />}></Route>
-				</Routes>
-			</BrowserRouter>
+        <NavBar />
+        <div className="app-main">
+            <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/auth" element={<Auth />}></Route>
+              <Route path="/migration" element={<Migration />}></Route>
+              {/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
+              <Route path="*" element={<NotFound />}></Route>
+            </Routes>
+          <Footer />
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
