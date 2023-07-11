a4aab27d33d73c670449c8a2caa0b2f291d96b9e
noooey <20203065@kookmin.ac.kr>
2023-07-11 18:08:23 +0900

    [feat/#3] :sparkles: Add: Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+function Footer() {
+  return (
+    <p
+        style={{
+            padding: '1vh 1vw',
+            color: '#dddddd',
+        }}
+    >
+        Copyright 2023. Harmonica. All rights reserved.
+    </p>
+  );
+}
+
+export default Footer;
