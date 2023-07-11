27ec3c86391fa86a2489276e388e3e925c566451
noooey <20203065@kookmin.ac.kr>
2023-07-11 18:13:00 +0900

    [feat/#3] :sparkles: Add: Hover, Navigator

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './NavBar.css';
 
 function NavBar() {
+  const navigate = useNavigate();
+
+  const handleClick = (event: React.MouseEvent<HTMLHeadingElement>) => {
+    navigate('/');
+  }
+
   return (
-    <div className="nav-bar" style={{height: '100vh'}}>
+    <div className="nav-bar">
       <div className="menu h-100">
-        <img src="icons/fi-rr-envelope.png" alt='envelop icon'/>
-        <img src="icons/fi-rr-home.png" alt='home icon' />
-        <img src="icons/fi-rr-network-cloud.png" alt='network cloud icon' />
-        <img src="icons/fi-rr-network.png" alt='network icon' />
-        <img src="icons/fi-rr-screen.png" alt='screen icon' />
-        <img src="icons/fi-rr-user.png" alt='user icon' />
+        <img src="icons/fi-rr-home.png" alt='home icon' onClick={handleClick} />
+        <img src="icons/fi-rr-envelope.png" alt='envelop icon' style={{pointerEvents:'none'}} />
+        <img src="icons/fi-rr-network-cloud.png" alt='network cloud icon' style={{pointerEvents:'none'}} />
+        <img src="icons/fi-rr-network.png" alt='network icon' style={{pointerEvents:'none'}} />
+        <img src="icons/fi-rr-screen.png" alt='screen icon' style={{pointerEvents:'none'}} />
+        <img src="icons/fi-rr-user.png" alt='user icon' style={{pointerEvents:'none'}} />
       </div>
     </div>
   );
