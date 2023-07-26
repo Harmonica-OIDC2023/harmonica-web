bad4ba969802c59f45f77fd5a04ddb2626f10c34
noooey <20203065@kookmin.ac.kr>
2023-07-26 16:10:17 +0900

    [feat/#9] :sparkles: Add: Migration Page

--- src/pages/Migration.tsx
+++ src/pages/Migration.tsx
@@ -1,80 +1,135 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import Container from 'react-bootstrap/Container';
 import ItemBlock from '../components/ItemBlock';
-import AuthForm from '../components/AuthForm';
+import { useDropzone } from 'react-dropzone';
+import { FiCheckCircle, FiHelpCircle } from 'react-icons/fi';
 import './Main.css';
 import NextButton from '../components/NextButton';
+import Hr from '../components/Hr';
 
 interface FormData {
-	fn_init: string;
-	func_file: File | null; // Specify the type as File | null
-  }
+  fn_init: string;
+  func_file: File | null; // Specify the type as File | null
+}
 
 const Migration = () => {
-	const [formData, setFormData] = useState<FormData>({
-		fn_init: '',
-		func_file: null
-	});
+  const [formData, setFormData] = useState<FormData>({
+    fn_init: '',
+    func_file: null,
+  });
 
-	const navigate = useNavigate();
-	const [isFormComplete, setIsFormComplete] = useState(false);
+  const [inputValue, setInputValue] = useState<File>();
 
-	const migrationHandler = () => {
-		// next 버튼 누르면 auth 페이지로 라우팅
-		if(isFormComplete) {
-			navigate('/completed');
-		}
-	};
+  const navigate = useNavigate();
+  const [isInitComplete, setIsInitComplete] = useState(false);
+  const [isUploadComplete, setIsUploadComplete] = useState(false);
 
-	const handleInputChange = (name: string, value: string | File) => {
-		setFormData((prevFormData) => ({
-		...prevFormData,
-		[name]: value,
-		}));
-	};
+  const initHandler = () => {
+    // init button handler
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      fn_init: 'completed',
+    }));
+  };
 
-	// Check if all form fields have a value
-	useEffect(() => {
-		const isComplete = Object.values(formData).every((value) => value !== '' && value !== null);
-		setIsFormComplete(isComplete);
-	}, [formData]);
+  const uploadHandler = () => {
+    // upload button handler
+    setIsUploadComplete(true);
+  };
 
-	return (
-		<div className="screen">
-			<ItemBlock style={{ width: '30%' }}>
-				auth process bar
-			</ItemBlock>
-			<ItemBlock style={{ display: 'flex', flexDirection: 'column' }}>
-				<div className="form-container" style={{ display: 'flex', flexDirection: 'column', width: '100%', marginBlockEnd: '5vh'}}>
-					<AuthForm
-						label="FN INIT"
-						name="fn_init"
-						value={formData.fn_init}
-						type="text"
-						onInputChange={handleInputChange}
-						infoLink='https://github.com/Harmonica-OIDC2023/harmonica-web'
-					/>
-					<AuthForm
-						label="FUNC_FILE"
-						name="func_file"
-						value={formData.func_file ? formData.func_file.name : ''}
-						type="file"
-						onInputChange={handleInputChange}
-						infoLink='https://github.com/Harmonica-OIDC2023/harmonica-web'
-					/>
-				</div>
-				<NextButton
-					disabled={!isFormComplete}
-					className={isFormComplete ? 'next-button-active' : 'next-button'}
-					onClick={migrationHandler}
-					style={{width: '25vw', height: '6vh', fontSize: '30px'}}
-				>
-					MIGRATION
-				</NextButton>
-			</ItemBlock>
-		</div>
-	);
+  const migrationHandler = () => {
+    // migration button handler
+    if (isInitComplete && isUploadComplete) {
+      navigate('/completed');
+    }
+  };
+
+  const handleFileDrop = (acceptedFiles: File[]) => {
+    if (acceptedFiles.length > 0) {
+      const file = acceptedFiles[0];
+      setInputValue(file);
+      setFormData(prevFormData => ({
+        ...prevFormData,
+        func_file: file,
+      }));
+    }
+  };
+
+  const { getRootProps, getInputProps } = useDropzone({ onDrop: handleFileDrop });
+
+  const renderInput = () => {
+    return (
+      <div {...getRootProps()} style={{ flex: '2', height: '80%', paddingLeft: '1vw', paddingRight: '1vw', border: '1.2px dashed #dddddd', color: '#9d9d9d', borderRadius: '5px', alignItems: 'center', display: 'flex', fontSize: '0.8rem' }}>
+        <input {...getInputProps()} />
+        {inputValue instanceof File ? (
+          <div style={{justifyContent: 'center', display: 'flex', width: '100%'}}>Selected file: {inputValue.name}</div>
+        ) : (
+          <div style={{justifyContent: 'center', display: 'flex', width: '100%'}}>{inputValue !== '' ? 'Selected file: ' + inputValue : 'Drag and drop the file here or click to select.'}</div>
+        )}
+      </div>
+    );
+  }
+
+  // Check if all form fields have a value
+  useEffect(() => {
+    setIsInitComplete(formData.fn_init === 'completed');
+    setIsUploadComplete(!!inputValue); // updated line
+  }, [formData, inputValue]); // added inputValue to the dependency array
+
+  return (
+    <div className="screen">
+      <ItemBlock style={{ width: '30%' }}>
+        auth process bar
+      </ItemBlock>
+      <ItemBlock style={{ display: 'flex', flexDirection: 'column' }}>
+        <div
+          style={{ display: 'flex', flex: 1, flexDirection: 'column', width: '100%', justifyContent: 'center', padding: '8px' }}
+        >
+          <div>
+            fn init을 해야합니다 어쩌구 저쩌구 설명
+          </div>
+          <NextButton
+            className={isInitComplete ? 'next-button-disabled' : 'next-button-active'}
+            onClick={initHandler}
+            disabled={isInitComplete}
+            style={{
+              width: '18vw',
+              height: '6vh',
+              fontSize: '30px',
+              borderRadius: '12px',
+              cursor: isInitComplete ? 'default' : 'pointer'
+            }}
+          >
+            {isInitComplete ? '✓ init' : 'init'}
+          </NextButton>
+        </div>
+        <Hr />
+        <div
+          style={{ display: 'flex', flex: 1, flexDirection: 'column', width: '100%', justifyContent: 'center', padding: '8px' }}
+        >
+          {renderInput()}
+          <div>
+            <div style={{ marginRight: 'auto', height: '100%', alignItems: 'center', marginBlockEnd: '0.15rem'}}>
+              {isUploadComplete ? <FiCheckCircle color="green" style={{ marginLeft: '0.5rem'}} /> : <FiCheckCircle color="#9d9d9d" style={{ marginLeft: '0.5rem'}} />}
+            </div>
+          </div>
+        </div>
+        <Hr />
+        <div
+          style={{ display: 'flex', flex: 1, flexDirection: 'column', width: '100%', justifyContent: 'center', padding: '8px' }}
+        >
+          <NextButton
+            disabled={!isInitComplete || !isUploadComplete}
+            className={(isInitComplete && isUploadComplete) ? 'next-button-active' : 'next-button'}
+            onClick={migrationHandler}
+            style={{ width: '18vw', height: '6vh', fontSize: '30px', borderRadius: '12px' }}
+          >
+            migration
+          </NextButton>
+        </div>
+      </ItemBlock>
+    </div>
+  );
 };
 
 export default Migration;
