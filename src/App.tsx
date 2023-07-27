75e99e2b6461bc50325fbdc54100300b9d42f0a7
noooey <20203065@kookmin.ac.kr>
2023-07-27 16:38:02 +0900

    [feat/#11] :art: Style: Position the Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 import Home from './pages/Home';
-import NotFound from './pages/NotFound';
-import Migration from './pages/Migration';
 import Auth from './pages/Auth';
 import Auth2 from './pages/Auth2';
-import Footer from './components/Footer';
+import Migration from './pages/Migration';
+import Completed from './pages/Completed';
+import NotFound from './pages/NotFound';
 import './App.css';
 
 function App() {
@@ -15,14 +16,17 @@ function App() {
       <BrowserRouter>
         <NavBar />
         <div className="app-main">
-            <Routes>
-              <Route path="/" element={<Home />}></Route>
-              <Route path="/auth" element={<Auth />}></Route>
-              <Route path="/auth2" element={<Auth2 />}></Route>
-              <Route path="/migration" element={<Migration />}></Route>
-              {/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
-              <Route path="*" element={<NotFound />}></Route>
-            </Routes>
+            <div style={{ width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '0vh 1vw 6vh 1vw'}}>
+              <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/auth" element={<Auth />}></Route>
+                <Route path="/auth2" element={<Auth2 />}></Route>
+                <Route path="/migration" element={<Migration />}></Route>
+                <Route path="/completed" element={<Completed />}></Route>
+                {/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
+                <Route path="*" element={<NotFound />}></Route>
+              </Routes>
+            </div>
           <Footer />
         </div>
       </BrowserRouter>
