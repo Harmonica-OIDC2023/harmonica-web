620aac9724f455b8b6ce3b06f3726defba325877
noooey <20203065@kookmin.ac.kr>
2023-07-27 16:33:53 +0900

    [feat/#11] :sparkles: Add: In Progress Spinner Component

---
+++ src/components/InProgressSpinner.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Lottie from 'lottie-react';
+import animationData from './lottie/animation_lkkrf2sp.json'; 
+
+const InProgressSpinner = () => {
+
+    const style = {
+        width: '40vw',
+    };
+
+    return (
+        <div style={{height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', padding: '0 0 20vh 0'}}>
+            <Lottie 
+                animationData={animationData}
+                loop={true} 
+                autoplay={true}
+                style={style} 
+                rendererSettings={{preserveAspectRatio: 'xMidYMid slice'}}
+            />
+            <img src="harmonica.png" alt='home icon' style={{width: '15vw', position: 'absolute', bottom: '35vh'}}/>
+            <div
+                style={{
+                    color: '#4b4b4b',
+                    textAlign: 'center',
+                    fontSize: '1vw'
+                }}
+            >
+                In progress...🚀
+            </div>
+        </div>
+    );
+};
+
+export default InProgressSpinner;
