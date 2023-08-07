da19e9dd922bd81c22c032305e963ed65b22a79a
noooey <20203065@kookmin.ac.kr>
2023-08-07 22:26:46 +0900

    [feat/#18] :sparkles: Add: Settings for Code Highlighting

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -5,3 +5,13 @@ declare module "*.json" {
     export default value;
 }
 declare module 'js-yaml';
+
+declare module 'react-syntax-highlighter' {
+    let Prism: any;
+    export { Prism };
+  }
+  
+  declare module 'react-syntax-highlighter/dist/esm/styles/prism' {
+    let solarizedlight: any;
+    export { solarizedlight };
+  }
