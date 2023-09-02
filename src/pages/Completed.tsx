f2fcc23130b9ecee9b8fbabfe323442181468233
noooey <20203065@kookmin.ac.kr>
2023-09-02 15:53:39 +0900

    [feat/#21] 🎨 Style: Edit animation state

--- src/pages/Completed.tsx
+++ src/pages/Completed.tsx
@@ -4,7 +4,7 @@ import Lottie from 'lottie-react';
 import animationData from '../components/lottie/animation_pink_check.json'; 
 
 const Completed = () => {
-    const [showCompleted, setShowCompleted] = useState(false);
+    const [showCompleted, setShowCompleted] = useState(true);
     const [opacity, setOpacity] = useState(0);
     const [endpoint, setEndpoint] = useState<string | undefined>();
     const navigate = useNavigate();
